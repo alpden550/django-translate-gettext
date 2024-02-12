5f8eaf618047854716fe7de46a2931942735c254
Denis Novikov <alpden550@gmail.com>
2024-02-12 09:52:10 +0300

    fix(app): fix app name

--- django_translate_gettext/apps.py
+++ django_translate_gettext/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class DjangoTranslateConfig(AppConfig):
+class DjangoTranslateGettextConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "django_translate"
+    name = "django_translate_gettext"
