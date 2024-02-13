65cb4b99e3e59e4922b54c0120b96183a239f6bc
Denis Novikov <alpden550@gmail.com>
2024-02-13 10:56:50 +0300

    feat(services): add getting all app models func

---
+++ django_translate_gettext/services/models.py
@@ -0,0 +1,18 @@
+from django.db.models import Model
+
+
+def get_all_app_models(app_label) -> set[Model]:
+    """
+    Get all models for the app label including the abstract models.
+    Args:
+        app_label (str): The app label to get the models for.
+
+    Returns:
+        set[Model]: The set of models for the app label.
+    """
+    result = set()
+    generation = {Model}
+    while generation:
+        generation = {sc for c in generation for sc in c.__subclasses__()}
+        result.update([c for c in generation if c._meta.app_label == app_label])
+    return result
