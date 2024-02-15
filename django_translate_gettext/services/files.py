446a3af0329c4dbab0f0fa06f0801366980e8ab1
Denis Novikov <alpden550@gmail.com>
2024-02-15 12:18:38 +0300

    feat(files): add recursive getting files from app name

--- django_translate_gettext/services/files.py
+++ django_translate_gettext/services/files.py
@@ -1,32 +1,39 @@
 import ast
-import subprocess
+from collections.abc import Iterator
 from pathlib import Path
 
-from loguru import logger
-
+from django_translate_gettext.constants import TO_SKIP
 from django_translate_gettext.services.transformers import ClassDefTransformer
 
 
-def update_py_file(*, file_path: str, formatted: bool = False) -> None:
+def fetch_app_files(app_name: str) -> Iterator[Path]:
+    """
+    Fetch all python files in the app directory excluding the files in the TO_SKIP list.
+    Args:
+        app_name (str): The app name to fetch the files from.
+
+    Returns:
+
+    """
+
+    all_files = {file for file in Path(app_name).rglob("*.py") if file.is_file()}
+    return filter(lambda x: not any(skip in str(x) for skip in TO_SKIP), all_files)
+
+
+def update_py_file(*, file_path: Path) -> None:
     """
     Update the python file with the gettext call wrapping.
     Args:
-        file_path (str): The file path to update.
-        formatted (bool): If True, format the code after updating the file.
+        file_path (Path): The file path Pathlib object to update.
 
     Returns:
         None
     """
-    model_file = Path(file_path).with_suffix(".py").absolute()
-    tree = ast.parse(model_file.read_text())
+    tree = ast.parse(file_path.read_text())
 
     transformer = ClassDefTransformer()
     new_tree = transformer.visit(tree)
     new_tree = transformer.insert_getetxt_import(new_tree)
 
     code = ast.unparse(new_tree)
-    model_file.write_text(code)
-
-    if formatted:
-        logger.info(f"Formatting the code for file {model_file!s}")
-        subprocess.run(["ruff", "format", str(model_file)], check=True)  # noqa: S603, S607
+    file_path.write_text(code)
