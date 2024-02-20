0e2d75f853ad099dcc82c2c00c71c96ca2b10e99
Denis Novikov <alpden550@gmail.com>
2024-02-20 15:28:42 +0300

    fix(models): add missing annotations

--- django_translate_gettext/services/models.py
+++ django_translate_gettext/services/models.py
@@ -1,9 +1,9 @@
 from django.db.models import Model
 
 
-def get_all_app_models(app_label) -> set[Model]:
-    """
-    Get all models for the app label including the abstract models.
+def get_all_app_models(app_label: str) -> set[Model]:
+    """Get all models for the app label including the abstract models.
+
     Args:
         app_label (str): The app label to get the models for.
 
@@ -14,5 +14,5 @@ def get_all_app_models(app_label) -> set[Model]:
     generation = {Model}
     while generation:
         generation = {sc for c in generation for sc in c.__subclasses__()}
-        result.update([c for c in generation if c._meta.app_label == app_label])
+        result.update([c for c in generation if c._meta.app_label == app_label])  # noqa: SLF001
     return result
