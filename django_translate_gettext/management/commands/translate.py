f30cd08fd4883bc3994df1ef8cee7d262ec66e0b
Denis Novikov <alpden550@gmail.com>
2024-03-18 09:33:03 +0300

    feat(management): add option to make po file and translate it

--- django_translate_gettext/management/commands/translate.py
+++ django_translate_gettext/management/commands/translate.py
@@ -4,10 +4,11 @@ from pathlib import Path
 
 from django.apps import apps
 from django.core.management.base import BaseCommand
-from loguru import logger
 
+from django_translate_gettext.exceptions import TranslatorError
 from django_translate_gettext.services import update_py_file
 from django_translate_gettext.services.files import fetch_app_files
+from django_translate_gettext.services.translators import PoFileTranslator
 
 
 class Command(BaseCommand):
@@ -22,6 +23,15 @@ class Command(BaseCommand):
             "--format",
             action="store_true",
             help="Call Ruff formatting tool to format the code after generating new model files.",
+        )
+        parser.add_argument(
+            "-mm",
+            "--makemessages",
+            nargs="+",
+            type=str,
+            help="Call makemessages command to create the .po files after adding the gettext "
+            "and translating for the passed languages."
+            "\nFor example: en de fr",
         )
 
     def handle(self, **options) -> None:
@@ -40,20 +50,41 @@ class Command(BaseCommand):
                 "to the database after adding the gettext."
             )
         )
-        self.stdout.write(
-            self.style.SUCCESS(
-                "Please, un the command 'python manage.py makemessages -l {lang code}' to create the .po files."
-            )
-        )
 
-    @staticmethod
-    def process_py_file(*, file_path: Path, formatted=True) -> None:
+        self.process_translating(**options)
+
+    def translate_lang_code(self, *, lang_code: str) -> None:
+        try:
+            translator = PoFileTranslator(lang_code=lang_code)
+            translator.translate_codes()
+            self.stdout.write(self.style.SUCCESS(f"Successfully translated for lang code {lang_code}."))
+        except TranslatorError as error:
+            self.stdout.write(self.style.ERROR(error))
+
+    def process_translating(self, **options) -> None:
+        if options["makemessages"]:
+            self.stdout.write(self.style.WARNING("Calling makemessages command to create the .po files."))
+            langs = [f"--locale={lang}" for lang in options["makemessages"]]
+            with suppress(subprocess.CalledProcessError):
+                subprocess.run(["python", "manage.py", "makemessages", *langs], check=True)  # noqa: S603, S607
+
+            for code in options["makemessages"]:
+                self.translate_lang_code(lang_code=code)
+
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Please, un the command 'python manage.py makemessages -l {lang code}' to create the .po files."
+                )
+            )
+
+    def process_py_file(self, *, file_path: Path, formatted=True) -> None:
         with suppress(FileNotFoundError):
             update_py_file(file_path=file_path)
             filename = file_path.absolute()
             if formatted:
-                logger.info(f"Formatting the code for files in app {filename!s}")
                 subprocess.run(["ruff", "format", f"{filename!s}"], check=True)  # noqa: S603, S607
+                self.stdout.write(self.style.SUCCESS(f"Formatted the code for files in app {filename!s}"))
 
     def process_app_files(self, *, app_name: str, **options) -> None:
         for file_path in fetch_app_files(app_name=app_name):
