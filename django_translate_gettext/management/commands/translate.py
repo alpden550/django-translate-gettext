da93aecc9915ae6f516a01afd76001b972cef379
Denis Novikov <alpden550@gmail.com>
2024-03-18 09:40:04 +0300

    V0.3.0 (#2)
    * refact(files): remove file formatting
    
    * refact(commands): add file formatting
    
    * fix(transformers): fix building args when call function exists
    
    * chore(poetry): add complexipy
    
    * refact(transformers): reduce methods complexity
    
    * chore(poetry): add deep translator
    
    * chore(readmi): update about new features
    
    * feat(exceptions): add base translator error
    
    * feat(services): add po file translator
    
    * feat(management): add option to make po file and translate it
    
    * chore(poetry): update package version

--- django_translate_gettext/management/commands/translate.py
+++ django_translate_gettext/management/commands/translate.py
@@ -1,10 +1,14 @@
+import subprocess
 from contextlib import suppress
+from pathlib import Path
 
 from django.apps import apps
 from django.core.management.base import BaseCommand
 
+from django_translate_gettext.exceptions import TranslatorError
 from django_translate_gettext.services import update_py_file
 from django_translate_gettext.services.files import fetch_app_files
+from django_translate_gettext.services.translators import PoFileTranslator
 
 
 class Command(BaseCommand):
@@ -15,9 +19,19 @@ class Command(BaseCommand):
             "apps", nargs="+", type=str, help="Apps to add gettext for the model files.\nFor example: app1 app2 app3"
         )
         parser.add_argument(
+            "-f",
             "--format",
             action="store_true",
             help="Call Ruff formatting tool to format the code after generating new model files.",
+        )
+        parser.add_argument(
+            "-mm",
+            "--makemessages",
+            nargs="+",
+            type=str,
+            help="Call makemessages command to create the .po files after adding the gettext "
+            "and translating for the passed languages."
+            "\nFor example: en de fr",
         )
 
     def handle(self, **options) -> None:
@@ -36,15 +50,44 @@ class Command(BaseCommand):
                 "to the database after adding the gettext."
             )
         )
-        self.stdout.write(
-            self.style.SUCCESS(
-                "Please, un the command 'python manage.py makemessages -l {lang code}' to create the .po files."
+
+        self.process_translating(**options)
+
+    def translate_lang_code(self, *, lang_code: str) -> None:
+        try:
+            translator = PoFileTranslator(lang_code=lang_code)
+            translator.translate_codes()
+            self.stdout.write(self.style.SUCCESS(f"Successfully translated for lang code {lang_code}."))
+        except TranslatorError as error:
+            self.stdout.write(self.style.ERROR(error))
+
+    def process_translating(self, **options) -> None:
+        if options["makemessages"]:
+            self.stdout.write(self.style.WARNING("Calling makemessages command to create the .po files."))
+            langs = [f"--locale={lang}" for lang in options["makemessages"]]
+            with suppress(subprocess.CalledProcessError):
+                subprocess.run(["python", "manage.py", "makemessages", *langs], check=True)  # noqa: S603, S607
+
+            for code in options["makemessages"]:
+                self.translate_lang_code(lang_code=code)
+
+        else:
+            self.stdout.write(
+                self.style.SUCCESS(
+                    "Please, un the command 'python manage.py makemessages -l {lang code}' to create the .po files."
+                )
             )
-        )
+
+    def process_py_file(self, *, file_path: Path, formatted=True) -> None:
+        with suppress(FileNotFoundError):
+            update_py_file(file_path=file_path)
+            filename = file_path.absolute()
+            if formatted:
+                subprocess.run(["ruff", "format", f"{filename!s}"], check=True)  # noqa: S603, S607
+                self.stdout.write(self.style.SUCCESS(f"Formatted the code for files in app {filename!s}"))
 
     def process_app_files(self, *, app_name: str, **options) -> None:
-        for filepath in fetch_app_files(app_name=app_name):
-            with suppress(FileNotFoundError):
-                update_py_file(file_path=filepath, formatted=options["format"])
+        for file_path in fetch_app_files(app_name=app_name):
+            self.process_py_file(file_path=file_path, formatted=options["format"])
 
         self.stdout.write(self.style.SUCCESS("Successfully added gettext for app files."))
